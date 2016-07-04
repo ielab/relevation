b2a5852b7dad34bcbd821566746254b03fa5be85
Bevan Koopman <bevan@koopman.id.au>
2016-07-04 15:30:35 +1000

    Revert "Authentacation and External Documents Support"

--- judgementapp/models.py
+++ judgementapp/models.py
@@ -1,98 +1,60 @@
 from django.conf import settings
 from django.db import models
 
-import doc_loader
-
 # Create your models here.
 
 
 class Document(models.Model):
 	docId = models.CharField(max_length=250)
-
-	# document loader dependent on settings
-	loader_cls = settings.DOCUMENT_TYPE + "Loader"
-	loader_inst = getattr(doc_loader, loader_cls)()
+	text = models.TextField()
 
 	def __unicode__(self):
 		return self.docId
 
 	def get_content(self):
-		return Document.loader_inst.get_content(self.docId)
-
-	def get_cashed_url(self):
-		return Document.loader_inst.get_cashed_url(self.docId)
+		content = ""
+		try:
+			with open(settings.DATA_DIR+"/"+self.docId) as f:
+				content = f.read()
+		except Exception:
+			content = "Could not read file %s" % settings.DATA_DIR+"/"+self.docId
+		return content
 
 class Query(models.Model):
 	qId = models.IntegerField()
 	text = models.CharField(max_length=250)
-	# confidence = models.IntegerField(blank=True, null=True)
-	# difficulty = models.IntegerField(blank=True, null=True)
-	# comment = models.TextField(blank=True, null=True)
-	# description = models.TextField(blank=True, null=True)
-	# criteria = models.TextField(blank=True, null=True)
+	difficulty = models.IntegerField(blank=True, null=True)
+	comment = models.TextField(blank=True, null=True)
 
+	instructions = models.TextField(blank=True, null=True)
+	criteria = models.TextField(blank=True, null=True)
 	example = models.TextField(blank=True, null=True)
 
 	def __unicode__(self):
 		return '%s: %s' % (self.qId, self.text)
 
 	def num_unjudged_docs(self):
-		return self.num_judgements() - len(self.judgements())
+		unjugded = [judgement for judgement in self.judgements() if judgement.relevance < 0]
+		return len(unjugded)
 
 	def num_judgements(self):
-		return len(self.judgement_templates())
-
-	def prepare_judgements(self, userid):
-		self._judgements = [judgement for judgement in Judgement.objects.filter(user=userid, query=self.id) if judgement.relevance != -1]
+		return len(self.judgements())
 
 	def judgements(self):
-		return self._judgements
-
-	def judgement_templates(self):
-		return JudgementTemplate.objects.filter(query=self.id)
-
-class QuerySurvey(models.Model):
-	query = models.ForeignKey(Query)
-	user = models.ForeignKey(settings.AUTH_USER_MODEL)
-	
-	confidence = models.IntegerField(blank=True, null=True)
-	difficulty = models.IntegerField(blank=True, null=True)
-	comment = models.TextField(blank=True, null=True)
-	description = models.TextField(blank=True, null=True)
-	criteria = models.TextField(blank=True, null=True)
-
-	def __unicode__(self):
-		return '%s: %s' % (self.query.qId, self.user.username)
-
-
-class JudgementTemplate(models.Model):
-	query = models.ForeignKey(Query)
-	document = models.ForeignKey(Document)
-
-	def __unicode__(self):
-		return '%s\t%s\n' % (self.query.qId, self.document.docId)
+		return Judgement.objects.filter(query=self.id)
 
 class Judgement(models.Model):
 
-	labels = {
-		-2: "Junk", 
-		-1: 'Unjudged', 
-		0: 'Not relvant', 
-		1: 'Relevant', 
-		2: 'Highly relevant',
-		3: 'Key Result',
-		4: 'Navigational'
-	}
+	labels = {-1: 'Unjudged', 0: 'Not relvant', 1: 'Somewhat relevant', 2:'Highly relevant'}
 
 	query = models.ForeignKey(Query)
-	user = models.ForeignKey(settings.AUTH_USER_MODEL)
 	document = models.ForeignKey(Document)
 	comment = models.TextField(blank=True, null=True)
 
 	relevance = models.IntegerField()
 
 	def __unicode__(self):
-		return '%s\t%s\t%s\t%s\n' % (self.query.qId, self.user.username, self.document.docId, self.relevance)
+		return '%s\t0%s\t%s\n' % (self.query.qId, self.document.docId, self.relevance)
 
 
 	def label(self):
--- judgementapp/views.py
+++ judgementapp/views.py
@@ -1,114 +1,55 @@
 # Create your views here.
 import cStringIO as StringIO
-from wsgiref.util import FileWrapper
-from django import forms
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.template import Context, loader, RequestContext
-from django.shortcuts import render_to_response, get_object_or_404, render, redirect
-
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
-from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render_to_response, get_object_or_404
+from django.core.servers.basehttp import FileWrapper
 
 from judgementapp.models import *
 
-@login_required
 def index(request):
     queries = Query.objects.order_by('qId')
     output = ', '.join([q.text for q in queries])
 
     template = loader.get_template('judgementapp/index.html')
-    context = RequestContext(request)
-    context['queries'] = queries
-    # context = Context({
-    #     'queries': queries,
-    # })
+    context = Context({
+        'queries': queries,
+    })
     return HttpResponse(template.render(context))
 
-@login_required
 def qrels(request):
     judgements = Judgement.objects.exclude(relevance=-1)
 
 
-    response = HttpResponse(judgements, content_type='application/force-download')
+    response = HttpResponse(judgements, mimetype='application/force-download')
     response['Content-Disposition'] = 'attachment; filename=qrels.txt'
     #response['X-Sendfile'] = myfile
     # It's usually a good idea to set the 'Content-Length' header too.
     # You can also set any other required headers: Cache-Control, etc.
     return response
 
-@login_required
 def query_list(request):
     queries = Query.objects.order_by('qId')
-    user = request.user
-    
-    unclaimed_queries = []
-    claimed_queries = []
-    for query in queries:
-        claimed_users = set(Judgement.objects.filter(query=query).values_list('user', flat=True))
-        if len(claimed_users) < settings.MAX_USER_PER_QUERY:
-            unclaimed_queries.append(query)
-        if user.id in claimed_users:
-            query.prepare_judgements(user.id)
-            claimed_queries.append(query)
 
-    return render_to_response('judgementapp/query_list.html', 
-        { 'queries': unclaimed_queries, 'claimed_queries': claimed_queries }, 
-        context_instance=RequestContext(request))
+    return render_to_response('judgementapp/query_list.html', { 'queries': queries}, context_instance=RequestContext(request))
 
-@login_required
 def query(request, qId):
     query = Query.objects.get(qId=qId)
-    user = request.user
+    judgements = Judgement.objects.filter(query=query.id)
 
-    judgements = Judgement.objects.filter(user=user.id, query=query.id)
-    survey, created = QuerySurvey.objects.get_or_create(user=user, query=query)
-
-    update = False
     if "difficulty" in request.POST:
-        survey.difficulty = int(request.POST['difficulty'])
-        update = True
-    if "confidence" in request.POST:
-        survey.confidence = int(request.POST['confidence'])
-        update = True
-    if "comment" in request.POST:
-        survey.comment = request.POST['comment']
-        update = True
-    if 'description' in request.POST:
-        survey.description = request.POST['description']
-        update = True
-    if 'criteria' in request.POST:
-        survey.criteria = request.POST['criteria']
-        update = True
-    if update:
-        survey.save()
+        query.difficulty = int(request.POST['difficulty'])
+        if "comment" in request.POST:
+            query.comment = request.POST['comment']
+        query.save()
 
     query.length = len(query.text)
 
-    return render_to_response('judgementapp/query.html', {'query': query, 'judgements': judgements, 'survey': survey},
+    return render_to_response('judgementapp/query.html', {'query': query, 'judgements': judgements}, 
         context_instance=RequestContext(request))
 
-@login_required
-def claim(request, qId):
-    query = Query.objects.get(qId=qId)
-    user = request.user
 
-    judgement_templates = JudgementTemplate.objects.filter(query=query.id)
-    judgements = Judgement.objects.filter(user=user.id, query=query.id)
-    if len(judgements) == 0:
-        print "Creating judgements..."
-        for template in judgement_templates:
-            judgement = Judgement()
-            judgement.user = user
-            judgement.query = query
-            judgement.document = template.document
-            judgement.relevance = -1
-            judgement.save()
-        judgements = Judgement.objects.filter(user=user.id, query=query.id)
-    return redirect(query_list)
-
-@login_required
 def document(request, qId, docId):
     document = Document.objects.get(docId=docId)
     query = Query.objects.get(qId=qId)
@@ -120,6 +61,7 @@ def document(request, qId, docId):
         if j.id == judgement.id:
             rank = count+1
             break
+
 
     prev = None
     try:
@@ -134,39 +76,38 @@ def document(request, qId, docId):
         pass
 
     content = document.get_content()
-    cached = document.get_cashed_url()
 
-    return render_to_response('judgementapp/document.html', {'document': document, 'query': query, 'judgement': judgement,
-        'next': next, 'prev': prev, 'rank': rank, 'total_rank': judgements.count(), 'content': content.strip(), 'cached': cached}, context_instance=RequestContext(request))
+    return render_to_response('judgementapp/document.html', {'document': document, 'query': query, 'judgement': judgement, 
+        'next': next, 'prev': prev, 'rank': rank, 'total_rank': judgements.count(), 'content': content.strip()}, context_instance=RequestContext(request))
 
-@login_required
 def judge(request, qId, docId):
     query = get_object_or_404(Query, qId=qId)
     document = get_object_or_404(Document, docId=docId)
-    user = request.user
     relevance = request.POST['relevance']
     comment = request.POST['comment']
 
-    judgements = Judgement.objects.filter(user=user.id, query=query.id)
-    judgement, created = Judgement.objects.get_or_create(user=user.id, query=query.id, document=document.id)
+    judgements = Judgement.objects.filter(query=query.id)
+    judgement, created = Judgement.objects.get_or_create(query=query.id, document=document.id)
     judgement.relevance = int(relevance)
     if comment != 'Comment':
         judgement.comment = comment
     judgement.save()
 
+    
+
     next = None
     try:
-        next = Judgement.objects.filter(user=user.id, query=query.id).get(id=judgement.id+1)
+        next = Judgement.objects.filter(query=query.id).get(id=judgement.id+1)
         if 'next' in request.POST:
             document = next.document
             judgement = next
-            next = Judgement.objects.filter(user=user.id, query=query.id).get(id=judgement.id+1)
+            next = Judgement.objects.filter(query=query.id).get(id=judgement.id+1)
     except:
         pass
 
     prev = None
     try:
-        prev = Judgement.objects.filter(user=user.id, query=query.id).get(id=judgement.id-1)
+        prev = Judgement.objects.filter(query=query.id).get(id=judgement.id-1)
     except:
         pass
 
@@ -176,12 +117,13 @@ def judge(request, qId, docId):
             rank = count+1
             break
 
+
     content = document.get_content()
 
-    return render_to_response('judgementapp/document.html', {'document': document, 'query': query, 'judgement': judgement,
+    return render_to_response('judgementapp/document.html', {'document': document, 'query': query, 'judgement': judgement, 
         'next': next, 'prev': prev, 'rank': rank, 'total_rank': judgements.count(), 'content': content.strip()}, context_instance=RequestContext(request))
 
-@staff_member_required
+
 def upload(request):
     context = {}
     if 'queryFile' in request.FILES:
@@ -191,7 +133,8 @@ def upload(request):
         for query in f:
             qid, txt = query.split("\t", 1)
             qryCount = qryCount + 1
-            query, created = Query.objects.get_or_create(qId=qid,text=txt)
+            query = Query(qId=qid,text=txt)
+            query.save()
         context['queries'] = qryCount
 
     if 'resultsFile' in request.FILES:
@@ -201,33 +144,28 @@ def upload(request):
         for result in f:
             qid, z, doc, rank, score, desc = result.split()
             docCount = docCount + 1
-
+            doc = doc.replace('corpus/', '')
+            
             document, created = Document.objects.get_or_create(docId=doc)
+            document.text = "TBA"
 
             query = Query.objects.get(qId=qid)
             document.save()
 
-            judgement_template = JudgementTemplate()
-            judgement_template.query = query
-            judgement_template.document = document
-
-            judgement_template.save()
-
+            judgement = Judgement()
+            judgement.query = query
+            judgement.document = document
+            judgement.relevance = -1
+            
+            judgement.save()
+                
         context['results'] = docCount
 
     return render_to_response('judgementapp/upload.html', context)
 
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            new_user = form.save()
-            return HttpResponseRedirect("/")
-    else:
-        form = UserCreationForm()
-    return render(request, "registration/signup.html", {
-        'form': form,
-    })
+
+
+
 
 
 
